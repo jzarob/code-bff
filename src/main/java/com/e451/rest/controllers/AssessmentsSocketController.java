42dce5e2920eaff8d0303fb3f67a356c7c97f58b
Jake Zarobsky <Jake.Zarobsky@8451.com>
2017-06-29 08:52:22 -0400

    Added inital websocket configuration classes

---
+++ src/main/java/com/e451/rest/controllers/AssessmentsSocketController.java
@@ -0,0 +1,11 @@
+package com.e451.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by j747951 on 6/29/2017.
+ */
+@Controller
+public class AssessmentsSocketController {
+    
+}
